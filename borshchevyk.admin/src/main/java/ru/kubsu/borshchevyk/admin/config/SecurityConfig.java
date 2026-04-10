91f8b5db8ae3e679fc390c80f3aec127b4ad9c6d
Aleksej Timko <aleksejtmk@gmail.com>
2026-04-10 21:09:19 +0300

    feat: add Spring Boot Admin for monitoring

---
+++ borshchevyk.admin/src/main/java/ru/kubsu/borshchevyk/admin/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package ru.kubsu.borshchevyk.admin.config;
+
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.config.Customizer;
+
+@Configuration(proxyBeanMethods = false)
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final AdminServerProperties adminServer;
+
+    public SecurityConfig(AdminServerProperties adminServer) {
+        this.adminServer = adminServer;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+        successHandler.setTargetUrlParameter("redirectTo");
+        successHandler.setDefaultTargetUrl(this.adminServer.getContextPath() + "/");
+
+        http.authorizeHttpRequests((authorizeRequests) -> authorizeRequests
+                .requestMatchers(new AntPathRequestMatcher(this.adminServer.getContextPath() + "/assets/**")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher(this.adminServer.getContextPath() + "/login")).permitAll()
+                .anyRequest().authenticated()
+        ).formLogin((formLogin) -> formLogin
+                .loginPage(this.adminServer.getContextPath() + "/login")
+                .successHandler(successHandler)
+        ).logout((logout) -> logout
+                .logoutUrl(this.adminServer.getContextPath() + "/logout")
+        ).httpBasic(Customizer.withDefaults())
+         .csrf((csrf) -> csrf
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .ignoringRequestMatchers(
+                        new AntPathRequestMatcher(this.adminServer.getContextPath() + "/instances", "POST"),
+                        new AntPathRequestMatcher(this.adminServer.getContextPath() + "/instances/*", "DELETE"),
+                        new AntPathRequestMatcher(this.adminServer.getContextPath() + "/actuator/**")
+                )
+        );
+
+        return http.build();
+    }
+}
