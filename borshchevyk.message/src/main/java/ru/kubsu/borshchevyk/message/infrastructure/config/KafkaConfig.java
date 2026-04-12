3093df1a20b85ff41eb4e53ec79ea24cd4b4fb76
Aleksej Timko <aleksejtmk@gmail.com>
2026-04-12 14:04:11 +0300

    feat: add kafka dlq configs for sync and message services

---
+++ borshchevyk.message/src/main/java/ru/kubsu/borshchevyk/message/infrastructure/config/KafkaConfig.java
@@ -0,0 +1,22 @@
+package ru.kubsu.borshchevyk.message.infrastructure.config;
+
+import org.apache.kafka.common.TopicPartition;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.util.backoff.FixedBackOff;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public DefaultErrorHandler errorHandler(KafkaTemplate<String, String> template) {
+        DeadLetterPublishingRecoverer recoverer = new DeadLetterPublishingRecoverer(template,
+                (record, ex) -> new TopicPartition(record.topic() + ".DLT", record.partition()));
+
+        // Retry 3 times with 2 seconds interval
+        return new DefaultErrorHandler(recoverer, new FixedBackOff(2000L, 3));
+    }
+}
